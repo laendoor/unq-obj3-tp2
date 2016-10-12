904211d184bfe77e980202538694db45f1d84ab6
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2016-10-12 17:59:26 -0300

    tests del motor de refactor completados

--- src/test/scala/ar/edu/unq/refactor/numbers/RefactorComparisionSpec.scala
+++ src/test/scala/ar/edu/unq/refactor/numbers/RefactorComparisionSpec.scala
@@ -54,71 +54,87 @@ trait RefactorComparisionSpec extends BaseRefactorSpec {
     program shouldBe expectedProgram
   }
 
-//  "Refactor Lesser" should "$containAlwaysTrueComparisionWarningMessage if first number is lesser than second" in {
-//    val expressions = List(MkLesser(0, 1), MkLesser(1, 2), MkLesser(2, 3))
-//    val problems = expressions map TrueComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  it should "$containAlwaysFalseComparisionWarningMessage if first number is greater-or-equals than second" in {
-//    val expressions = List(MkLesser(0, 0), MkLesser(1, 0), MkLesser(1, 1), MkLesser(2, 1))
-//    val problems = expressions map FalseComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  "Refactor Greater" should "$containAlwaysTrueComparisionWarningMessage if first number is greater than second" in {
-//    val expressions = List(MkGreater(1, 0), MkGreater(2, 1), MkGreater(3, 2))
-//    val problems = expressions map TrueComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  it should "$containAlwaysFalseComparisionWarningMessage if first number is lesser-or-equals than second" in {
-//    val expressions = List(MkGreater(0, 0), MkGreater(0, 1), MkGreater(1, 1), MkGreater(1, 2))
-//    val problems = expressions map FalseComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  "Refactor LesserOrEquals" should "$containAlwaysTrueComparisionWarningMessage if first number is lesser-or-equal than second" in {
-//    val expressions = List(MkLesserOrEqual(0, 0), MkLesserOrEqual(0, 1), MkLesserOrEqual(1, 1), MkLesserOrEqual(1, 2))
-//    val problems = expressions map TrueComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  it should "$containAlwaysFalseComparisionWarningMessage if first number is greater than second" in {
-//    val expressions = List(MkLesserOrEqual(1, 0), MkLesserOrEqual(2, 1), MkLesserOrEqual(3, 2))
-//    val problems = expressions map FalseComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  "Refactor GreaterOrEquals" should "$containAlwaysTrueComparisionWarningMessage if first number is greater-or-equal than second" in {
-//    val expressions = List(
-//      MkGreaterOrEqual(0, 0), MkGreaterOrEqual(1, 0),
-//      MkGreaterOrEqual(1, 1), MkGreaterOrEqual(2, 1)
-//    )
-//    val problems = expressions map TrueComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
-//
-//  it should "$containAlwaysFalseComparisionWarningMessage if first number is lesser than second" in {
-//    val expressions = List(MkGreaterOrEqual(0, 1), MkGreaterOrEqual(1, 2), MkGreaterOrEqual(2, 3))
-//    val problems = expressions map FalseComparisonProblem
-//    val expectedProblems = CheckAllRules(Program(expressions))
-//
-//    problems.foreach(p => expectedProblems should contain (p))
-//  }
+  "Refactor Lesser" should "return True Booleans when first number is lesser than second" in {
+    val expressions = List(MkLesser(0, 1), MkLesser(1, 2), MkLesser(2, 3))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(3)(Boolean(true))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  it should "return False Booleans when first number is greater or equal than second" in {
+    val expressions = List(MkLesser(0, 0), MkLesser(1, 0), MkLesser(1, 1), MkLesser(2, 1))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(4)(Boolean(false))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  "Refactor Greater" should "return True Booleans when first number is greater than second" in {
+    val expressions = List(MkGreater(1, 0), MkGreater(2, 1), MkGreater(3, 2))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(3)(Boolean(true))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  it should "return False Booleans when first number is lesser or equal than second" in {
+    val expressions = List(MkGreater(0, 0), MkGreater(0, 1), MkGreater(1, 1), MkGreater(1, 2))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(4)(Boolean(false))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  "Refactor LesserOrEquals" should "return True Booleans when first number is lesser or equal than second" in {
+    val expressions = List(MkLesserOrEqual(0, 0), MkLesserOrEqual(0, 1), MkLesserOrEqual(1, 1), MkLesserOrEqual(1, 2))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(4)(Boolean(true))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  it should "return False Booleans when first number is greater than second" in {
+    val expressions = List(MkLesserOrEqual(1, 0), MkLesserOrEqual(2, 1), MkLesserOrEqual(3, 2))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(3)(Boolean(false))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  "Refactor GreaterOrEquals" should "return True Booleans when first number is greater or equal than second" in {
+    val expressions = List(
+      MkGreaterOrEqual(0, 0), MkGreaterOrEqual(1, 0),
+      MkGreaterOrEqual(1, 1), MkGreaterOrEqual(2, 1)
+    )
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(4)(Boolean(true))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
+
+  it should "return False Booleans when first number is lesser than second" in {
+    val expressions = List(MkGreaterOrEqual(0, 1), MkGreaterOrEqual(1, 2), MkGreaterOrEqual(2, 3))
+    val program = RefactorWithAllRules(Program(expressions))
+
+    val expectedExpressions = List.fill(3)(Boolean(false))
+    val expectedProgram = Program(expectedExpressions)
+
+    program shouldBe expectedProgram
+  }
 
 }
